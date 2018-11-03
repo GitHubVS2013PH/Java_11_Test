518755392f18c2f363c507b5f136c885b540dc9f
GitHubVS2013PH <phayter1@gmail.com>
2018-11-02 17:39:02 -0700

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Java 11 Test");
+	    System.out.println("Adding line to test push");
+	    System.out.println("Adding line 2 to test push");
+    }
+}
